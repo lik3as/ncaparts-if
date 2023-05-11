2d3fce8c6bf954fc39adf1e2325fbf5bf3581a15
Davi Almeida <lik3games@gmail.com>
2023-05-11 10:50:56 -0300

    Initial commit

---
+++ if-db_methods.ts
@@ -0,0 +1,31 @@
+import x from 'xlsx'
+
+
+
+export default {
+  insert_data,
+  substitute_data,
+  update_data,
+  update_path,
+  show_menu
+}
+
+function insert_data() {
+  
+}
+
+function substitute_data(){
+
+}
+
+function update_data(){
+
+}
+
+function update_path(){
+
+}
+
+function show_menu(){
+
+}
